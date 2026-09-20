72211a1702e765d6c2d1787b757bdcc81135a07b
890asif <192508892+890asif@users.noreply.github.com>
2026-09-20 10:51:50 +0600

    update bookcard

--- src/components/homePage/Books.tsx
+++ src/components/homePage/Books.tsx
@@ -12,13 +12,20 @@ const Books = async() => {
     const booksData = await getBooks();
     console.log(booksData)
     return (
-      <section className=' px-10 grid grid-cols-3 gap-6'>
-        {
+      <section>
+        <div className='text-center mb-10 space-y-2'>
+            <h3 className='text-[#00D390] font-bold'>Our Collections</h3>
+            <h2 className='text-3xl font-bold text-gray-800'>Explore Popular Books</h2>
+            <p className='text-gray-500'>Discover amazing stories, timeless classics, and inspiring books from talented authors.</p>
+        </div>
+            <div className=' px-10 grid grid-cols-3 gap-6'>
+                {
             booksData.map((book:IBook) =>{
                 return <BookCard key={book.bookId} book={book}></BookCard>
                 
             })
         }
+            </div>
       </section>
     );
 };
